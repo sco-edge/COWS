ef53baaab81b2497693842e6061a52abcede874c
junho <90366348+yellowCornSalad@users.noreply.github.com>
2024-06-26 19:50:46 +0900

    Update IstioManual.sh

--- IstioManual.sh
+++ IstioManual.sh
@@ -7,3 +7,13 @@ kubectl -n istio-system port-forward svc/grafana 3000:80
 kubectl port-forward svc/prometheus 9090:9090 -n istio-system
 kubectl -n istio-system port-forward deploy/prometheus 9090:9090
 kubectl -n istio-system port-forward svc/prometheus-server 9090:80
+
+## 현재 사용중인 마이크로 서비스 확인
+$ kubectl get namespaces
+NAME              STATUS   AGE
+default           Active   8d
+istio-bookinfo    Active   2d6h
+istio-system      Active   2d6h
+kube-node-lease   Active   8d
+kube-public       Active   8d
+kube-system       Active   8d
