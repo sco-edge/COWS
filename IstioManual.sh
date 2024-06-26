539e5e691a5a9cd4d5deee0e0437f61f79eb69e8
junho <90366348+yellowCornSalad@users.noreply.github.com>
2024-06-26 19:52:03 +0900

    Update IstioManual.sh

--- IstioManual.sh
+++ IstioManual.sh
@@ -8,7 +8,7 @@ kubectl port-forward svc/prometheus 9090:9090 -n istio-system
 kubectl -n istio-system port-forward deploy/prometheus 9090:9090
 kubectl -n istio-system port-forward svc/prometheus-server 9090:80
 
-## 현재 사용중인 마이크로 서비스 확인
+## 현재 사용중인 마이크로 서비스 확인(서비스 메시가 설치된 네임스페이스를 확인)
 $ kubectl get namespaces
 NAME              STATUS   AGE
 default           Active   8d
@@ -17,3 +17,14 @@ istio-system      Active   2d6h
 kube-node-lease   Active   8d
 kube-public       Active   8d
 kube-system       Active   8d
+
+## ISTIO 구성요소 확인(istio-bookinfo)
+$ kubectl get pods -n istio-bookinfo
+NAME                              READY   STATUS    RESTARTS      AGE
+details-v1-6c98bc7c4c-j6xw9       1/1     Running   1 (95m ago)   2d6h
+productpage-v1-6d56b97dfb-qmcx4   1/1     Running   2 (95m ago)   2d6h
+ratings-v1-7645575cf9-2t9ln       1/1     Running   1 (95m ago)   2d6h
+reviews-v1-6c5c97576c-8wmbj       1/1     Running   1 (95m ago)   2d6h
+reviews-v2-575d87db6c-6gtwt       1/1     Running   1 (95m ago)   2d6h
+reviews-v3-698c4b8d4c-rsbq7       1/1     Running   1 (95m ago)   2d6h
+
