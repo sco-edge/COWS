6704a952de5f9b56f33afef868c8bf9b468eb3ba
junho <90366348+yellowCornSalad@users.noreply.github.com>
2024-07-22 13:55:03 +0900

    Update install_istio.sh

--- install_istio.sh
+++ install_istio.sh
@@ -1,5 +1,11 @@
 ### istio 설치
-
 curl -L https://istio.io/downloadIstio | sh -
 cd istio-<version>
 export PATH=$PWD/bin:$PATH
+
+### istio 설치 파일 적용
+istioctl install --set profile=demo -y
+
+### Istio 시스템 네임스페이스와 기본 네임스페이스에 레이블 추가:
+
+kubectl label namespace default istio-injection=enabled
