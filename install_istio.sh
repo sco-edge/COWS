b117252a2529bde349e87e9e2e8d9ab16bec0b52
junho <90366348+yellowCornSalad@users.noreply.github.com>
2024-07-22 13:55:30 +0900

    Update install_istio.sh

--- install_istio.sh
+++ install_istio.sh
@@ -6,6 +6,9 @@ export PATH=$PWD/bin:$PATH
 ### istio 설치 파일 적용
 istioctl install --set profile=demo -y
 
-### Istio 시스템 네임스페이스와 기본 네임스페이스에 레이블 추가:
-
+### Istio 시스템 네임스페이스와 기본 네임스페이스에 레이블 추가
 kubectl label namespace default istio-injection=enabled
+
+### Bookinfo 애플리케이션 배포
+kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+
