d6a3d2e3175291c9e44efa5e8fe3010e38230668
junho <90366348+yellowCornSalad@users.noreply.github.com>
2024-07-26 16:01:38 +0900

    Update install_istio_Bookinfo.sh

--- install_istio_Bookinfo.sh
+++ install_istio_Bookinfo.sh
@@ -61,6 +61,8 @@ istioctl dashboard kiali
 ### Jaeger 대시보드
 istioctl dashboard jaeger
 kubectl -n istio-system port-forward deploy/jaeger 16686:16686
+### trace 추적
+kubectl -n istio-system port-forward svc/jaeger-query 16685:16686 -n istio-system
 
 ## Zipkin 접근
 kubectl -n istio-system port-forward svc/zipkin 9411:9411
