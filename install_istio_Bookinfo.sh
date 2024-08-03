585003761326191a0ec4f1ad90bb4a3e64ef75d3
Junho Bae <90366348+yellowCornSalad@users.noreply.github.com>
2024-08-03 06:41:50 +0000

    Update install_istio_Bookinfo.sh

--- install_istio_Bookinfo.sh
+++ install_istio_Bookinfo.sh
@@ -1,3 +1,7 @@
+### Docker 설치
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
 ### istio 설치
 curl -L https://istio.io/downloadIstio | sh -
 cd istio-<version>
