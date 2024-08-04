ae9dcbee7b375a7c1a828e4adc9d102d03112e12
Junho Bae <90366348+yellowCornSalad@users.noreply.github.com>
2024-08-04 19:05:19 +0900

    Update install_istio_Bookinfo.sh

--- install_istio_Bookinfo.sh
+++ install_istio_Bookinfo.sh
@@ -19,7 +19,7 @@ istioctl install --set profile=demo -y
 
 ### Istio 시스템 네임스페이스와 기본 네임스페이스에 레이블 추가
 kubectl label namespace default istio-injection=enabled
-cd Documents/istio-1.22.3/samples/
+cd Documents/istio-1.22.3/
 
 ### Bookinfo 애플리케이션 배포
 kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
