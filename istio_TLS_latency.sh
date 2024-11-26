4efee289a6cc22bddfee4dd4f35afd48c6820feb
Junho Bae <90366348+yellowCornSalad@users.noreply.github.com>
2024-11-26 15:34:16 +0900

    Update istio_TLS_latency.sh

--- istio_TLS_latency.sh
+++ istio_TLS_latency.sh
@@ -1,5 +1,9 @@
 ### 마이크로서비스 간 TLS로 인한 평균 지연 시간
-
 sum(rate(istio_request_duration_milliseconds_sum{security_istio_io_tlsMode="istio"}[1m])) 
 / 
 sum(rate(istio_request_duration_milliseconds_count{security_istio_io_tlsMode="istio"}[1m]))
+
+### TLS 껐을 때의 평균 지연 시간
+sum(rate(istio_request_duration_milliseconds_sum{security_istio_io_tlsMode!="istio"}[1m])) 
+/ 
+sum(rate(istio_request_duration_milliseconds_count{security_istio_io_tlsMode!="istio"}[1m]))
