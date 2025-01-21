8d516ce324d86954286ebdcbd4a985df667eb922
Junho Bae <90366348+yellowCornSalad@users.noreply.github.com>
2025-01-21 10:26:31 +0900

    Update wrk2.sh

--- wrk2.sh
+++ wrk2.sh
@@ -1,7 +1,13 @@
 sudo apt-get update
 sudo apt-get install build-essential libssl-dev git
 git clone https://github.com/giltene/wrk2.git
+
+### 애플 실리콘 환경에는 Rosetta 2 설치 필요
+sudo softwareupdate --install-rosetta
+
 cd wrk2
-make
-sudo cp wrk /usr/local/bin/wrk2
+make clean && make
+
+### path 설정
+sudo cp wrk /usr/local/bin
 
