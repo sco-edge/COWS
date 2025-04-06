e8f6977e343d25736fb1198c738bf2c22c727c7c
junho <90366348+yellowCornSalad@users.noreply.github.com>
2025-04-06 15:53:37 +0900

    server-key

--- gen.sh
+++ gen.sh
@@ -6,5 +6,5 @@ openssl req -x509 -newkey rsa:4096 -days 365 -keyout ca-key.pem -out ca-cert.pem
 echo "CA's self-signed certificate"
 openssl x509 -in ca-cert.pem -noout -text
 # Generate web server's private key and certificate signing request (CSR)
-
+openssl req -newkey rsa:4096 -keyout server-key.pem -out server-req.pem -subj "C = ko/ST = Seoul/L = Seoul/O = korea/OU = KU/CN = bjhbae@korea.ac.kr/emailAddress = bjhbae@korea.ac.kr"
 # Use CA's private key to sign web server's CSR and get back the signed certificate
