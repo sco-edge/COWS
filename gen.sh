71ec141d0d2161a9925fe4efa3173aca3147b9b6
junho <90366348+yellowCornSalad@users.noreply.github.com>
2025-04-06 15:47:16 +0900

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,6 +1,9 @@
-# Generate CA's private key and self-signed certificate
-openssl req -x509 -newkey rsa:4096 -days 365 -keyout ca-key.pem -out ca-cert.pem
+rm *.pem
 
+# Generate CA's private key and self-signed certificate
+openssl req -x509 -newkey rsa:4096 -days 365 -keyout ca-key.pem -out ca-cert.pem -subj "C = ko/ST = Seoul/L = Seoul/O = korea/OU = KU/CN = bjhbae@korea.ac.kr/emailAddress = bjhbae@korea.ac.kr"
+
+echo "CA's self-signed certificate"
 openssl x509 -in ca-cert.pem -noout -text
 # Generate web server's private key and certificate signing request (CSR)
 
