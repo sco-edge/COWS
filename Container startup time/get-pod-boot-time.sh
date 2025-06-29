8de4dcb45ed3b9636ef7617e1829f8ac6a8823d6
Junho Bae <90366348+yellowCornSalad@users.noreply.github.com>
2025-06-29 19:57:35 +0900

    Update get-pod-boot-time.sh

--- Container startup time/get-pod-boot-time.sh
+++ Container startup time/get-pod-boot-time.sh
@@ -8,11 +8,21 @@ pod="$1"
 get_condition_time() {
     condition="$1"
     iso_time=$(kubectl get pod "$pod" -o json | jq ".status.conditions[] | select(.type == \"$condition\" and .status == \"True\") | .lastTransitionTime" | tr -d '"\n')
-    date -d $iso_time +%s
+    # ISO8601 타임스탬프를 초.나노초 단위로 변환
+    date -d $iso_time +%s.%N
 }
 
-initialized_time=$(get_condition_time PodScheduled)
+pod_scheduled_time=$(get_condition_time PodScheduled)
+initialized_time=$(get_condition_time Initialized)
 ready_time=$(get_condition_time Ready)
-duration_seconds=$(( ready_time - initialized_time ))
+containers_ready_time=$(get_condition_time ContainersReady)
 
-echo "It took $duration_seconds seconds for $pod to boot up"
+# 각 단계별 소요 시간 계산 (소수점 셋째 자리까지)
+scheduled_to_initialized=$(echo "$initialized_time - $pod_scheduled_time" | bc)
+initialized_to_ready=$(echo "$ready_time - $initialized_time" | bc)
+ready_to_containers_ready=$(echo "$containers_ready_time - $ready_time" | bc)
+
+# 결과 출력
+printf "PodScheduled to Initialized: %.3f seconds\n" $scheduled_to_initialized
+printf "Initialized to Ready: %.3f seconds\n" $initialized_to_ready
+printf "Ready to ContainersReady: %.3f seconds\n" $ready_to_containers_ready
