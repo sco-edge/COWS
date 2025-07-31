4918f22aca9c6760940a3a476283c1eff01f2112
Junho Bae <90366348+yellowCornSalad@users.noreply.github.com>
2025-07-31 17:04:42 +0900

    Update able_mTLS.sh

--- able_mTLS/able_mTLS.sh
+++ able_mTLS/able_mTLS.sh
@@ -1,5 +1,5 @@
 kubectl delete peerauthentication disable-mtls -n istio-system
-
+### kubectl get peerauthentication -A
 ## 기존에 disable 되어있는 mtls를 다시 able 시키기 위해서는 대상의 name과 namespace의 이름이 일치해야 한다.
 
 kubectl apply -f - <<EOF
